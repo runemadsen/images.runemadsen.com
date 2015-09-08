85086382f1474464fe7e26a45232bce41f19d854
Rune Madsen <rune@runemadsen.com>
2015-09-08 11:02:35 -0400

    adding disk caching

--- main.go
+++ main.go
@@ -8,22 +8,38 @@ import (
 	"os"
 	"strings"
 
+	"github.com/gregjones/httpcache"
+	"github.com/gregjones/httpcache/diskcache"
+	"github.com/peterbourgon/diskv"
+
 	"willnorris.com/go/imageproxy"
 )
 
 func main() {
 
+	// Set server address
 	addr := os.Getenv("ADDRESS")
 	if addr == "" {
 		log.Fatal("No address provided for the imageproxy")
 	}
 
-	p := imageproxy.NewProxy(nil, nil)
+	// Set cache
+	var cache httpcache.Cache
+	d := diskv.New(diskv.Options{
+		BasePath:     "/tmp/imageproxy",
+		CacheSizeMax: 500 * 1024 * 1024,
+	})
+	cache = diskcache.NewWithDiskv(d)
 
+	// Create proxy
+	p := imageproxy.NewProxy(nil, cache)
+
+	// Create whitelist
 	if os.Getenv("WHITELIST") != "" {
 		p.Whitelist = strings.Split(os.Getenv("WHITELIST"), ",")
 	}
 
+	// Create baseurl
 	if os.Getenv("BASEURL") != "" {
 		var err error
 		p.DefaultBaseURL, err = url.Parse(os.Getenv("BASEURL"))
